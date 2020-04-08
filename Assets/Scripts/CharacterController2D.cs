bc9a5f761f0d927924dba1128efe6b18bd5ee40b
Issac <iirlas@yahoo.com>
2020-04-07 23:19:34 -0500

    Added Scripts
    Added Assets/Scripts/CharacterBehaviour.cs
    Added Assets/Scripts/CharacterController2D.cs
    Added Assets/Scripts/Movement.cs

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-1)]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    public enum CollisionType
+    {
+        Enter,
+        Exit,
+        Stay
+    }
+    public delegate void Collision2DProc(Collision2D collision, CollisionType type);
+    public delegate void Trigger2DProc(Collider2D collider, CollisionType type);
+
+    public LayerMask layer;
+    public event Collision2DProc OnCollision2D;
+    public event Trigger2DProc OnTrigger2D;
+    [HideInInspector] public new Rigidbody2D rigidbody2D;
+    [HideInInspector] public new Collider2D collider2D;
+
+    public Vector2 Position
+    {
+        get
+        {
+            return rigidbody2D.position;
+        }
+        set
+        {
+            rigidbody2D.position = value;
+        }
+    }
+
+    public void Move(Vector2 motion)
+    {
+        rigidbody2D.velocity = motion;
+    }
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        OnCollision2D?.Invoke(collision, CollisionType.Enter);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        OnCollision2D?.Invoke(collision, CollisionType.Exit);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        OnCollision2D?.Invoke(collision, CollisionType.Stay);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        OnTrigger2D?.Invoke(collider, CollisionType.Enter);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        OnTrigger2D?.Invoke(collider, CollisionType.Exit);
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        OnTrigger2D?.Invoke(collider, CollisionType.Stay);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public CharacterController2D characterController2D;
+    private Vector2 _direction;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector2>();
+    }
+
+    private void Awake()
+    {
+        characterController2D = GetComponentInParent<CharacterController2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 motion = new Vector2
+        {
+            x = _direction.x * speed,
+            y = _direction.y * speed
+        };
+        characterController2D.Move(motion);
+    }
+}
