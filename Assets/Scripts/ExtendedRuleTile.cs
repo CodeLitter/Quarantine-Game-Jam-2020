da1f036eb654dd63f9de3184169e255d3ed16de9
Issac <iirlas@yahoo.com>
2020-04-11 08:34:01 -0500

    Added Assets/Scripts/ExtendedRuleTile.cs

---
+++ Assets/Scripts/ExtendedRuleTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class ExtendedRuleTile : RuleTile
+{
+    public List<TileBase> siblings = new List<TileBase>();
+
+    public override bool RuleMatch(int neighbor, TileBase tile)
+    {
+        if (siblings.Contains(tile))
+        {
+            return base.RuleMatch(neighbor, this);
+        }
+        return base.RuleMatch(neighbor, tile);
+    }
+}
