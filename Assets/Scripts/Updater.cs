699f47c40b97f50b9da75bc4fb7f4c696db7c2d7
Issac <iirlas@yahoo.com>
2020-04-12 11:38:33 -0500

    Added Scorekeeping
    Added Assets/Fonts/Kalam-Regular.ttf
    Added Assets/Scripts/Updater.cs

---
+++ Assets/Scripts/Updater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Updater : MonoBehaviour
+{
+    public Spawner spawner;
+    public Text score;
+
+    private void Start()
+    {
+        StartCoroutine(UpdateScore());
+    }
+
+    private IEnumerator UpdateScore()
+    {
+        while (true)
+        {
+            int count = spawner.transform.Cast<Transform>().Count(child => !child.gameObject.activeSelf);
+            score.text = $"Alpacas {count}/{spawner.transform.childCount}";
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
