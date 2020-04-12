21f9c909c6203c96f39b614abc04eb17485a73cf
psipes <p.a.sipes@gmail.com>
2020-04-11 22:28:50 -0500

    Menu Scenes

---
+++ Assets/Scripts/SceneLoaderInt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneLoaderInt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeScenery(int sceneToLoad)
+    {
+        EditorSceneManager.LoadScene(sceneToLoad);
+    }
+}
