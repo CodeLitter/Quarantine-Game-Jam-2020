1fd4726c13dba3171e8f1ce0d20f08d240077476
Issac <iirlas@yahoo.com>
2020-04-12 05:26:48 -0500

    Added Spawner
    Revised ProjectSettings/TagManager.asset, added Alpaca Tag
    Revised Assets/Prefabs/AlpacaRainbow.prefab, Alpaca Tag
    Revised Assets/Prefabs/AlpacaWhite.prefab, Alpaca Tag

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> gameObjects;
+    public UnityEngine.Events.UnityEvent onFinish;
+
+    private void Start()
+    {
+        var spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (var spawnPoint in spawnPoints)
+        {
+            var choice = gameObjects[Random.Range(0, gameObjects.Capacity)];
+            var instance = Instantiate(choice, spawnPoint.transform.position, Quaternion.identity, transform);
+        }
+        StartCoroutine(Check());
+    }
+
+    private IEnumerator Check()
+    {
+        var childern = transform.Cast<Transform>().ToList();
+        while (childern.Any(child => child.gameObject.activeSelf))
+        {
+            yield return new WaitForSeconds(1.0f);
+        }
+        onFinish.Invoke();
+    }
+}
