d7d9afcae24a089522414762244a392dd70ad84a
Issac <iirlas@yahoo.com>
2020-04-12 05:30:58 -0500

    Added Ship
    Added Ship Animation
    Added Assets/Scripts/Disabler.cs
    
    Revised Ship Sprites Size
    Revised Assets/Scenes/SampleScene.unity, added ship

---
+++ Assets/Scripts/Disabler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disabler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Alpaca"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
